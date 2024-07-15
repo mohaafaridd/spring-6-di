4948734436a617af432a4e998d2454f4b3f120a8
Mohammed Farid <dev.mohammedfarid@gmail.com>
2024-07-15 09:51:54 +0300

    accessing controller bean from main method

--- src/main/java/mohaafaridd/dev/spring6di/Spring6diApplication.java
+++ src/main/java/mohaafaridd/dev/spring6di/Spring6diApplication.java
@@ -1,13 +1,24 @@
 package mohaafaridd.dev.spring6di;
 
+import mohaafaridd.dev.spring6di.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Spring6diApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Spring6diApplication.class, args);
+
+
+        ApplicationContext ctx = SpringApplication.run(Spring6diApplication.class, args);
+
+        MyController controller = ctx.getBean(MyController.class);
+
+        System.out.println("I'm in main method");
+
+        System.out.println(controller.sayHello());
     }
 
 }
---
+++ src/main/java/mohaafaridd/dev/spring6di/controllers/MyController.java
@@ -0,0 +1,11 @@
+package mohaafaridd.dev.spring6di.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+    public String sayHello() {
+        System.out.println("I'm in the controller");
+        return "Hello World";
+    }
+}
