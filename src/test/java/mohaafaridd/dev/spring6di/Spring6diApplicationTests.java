a3b6751d5ac1fcca010bdc5276d1eecd6883bb9c
Mohammed Farid <dev.mohammedfarid@gmail.com>
2024-07-15 09:55:40 +0300

    accessing controller inside spring boot test

--- src/test/java/mohaafaridd/dev/spring6di/Spring6diApplicationTests.java
+++ src/test/java/mohaafaridd/dev/spring6di/Spring6diApplicationTests.java
@@ -1,13 +1,34 @@
 package mohaafaridd.dev.spring6di;
 
+import mohaafaridd.dev.spring6di.controllers.MyController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class Spring6diApplicationTests {
 
+    @Autowired
+    ApplicationContext applicationContext;
+
+    @Autowired
+    MyController myController;
+
+    @Test
+    void testAutowiredOfController() {
+        System.out.println(myController.sayHello());
+    }
+
+    @Test
+    void testGetControllerFromContext() {
+        MyController controller = applicationContext.getBean(MyController.class);
+        System.out.println(controller.sayHello());
+    }
+
     @Test
     void contextLoads() {
+
     }
 
 }
