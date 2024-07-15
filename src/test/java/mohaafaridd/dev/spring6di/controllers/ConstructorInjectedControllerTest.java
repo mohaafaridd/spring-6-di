c34f60d96a2a372adc198f9aeaf1d0f65b2cc50d
Mohammed Farid <dev.mohammedfarid@gmail.com>
2024-07-15 10:28:40 +0300

    wiring spring components using annotations

--- src/test/java/mohaafaridd/dev/spring6di/controllers/ConstructorInjectedControllerTest.java
+++ src/test/java/mohaafaridd/dev/spring6di/controllers/ConstructorInjectedControllerTest.java
@@ -3,16 +3,15 @@ package mohaafaridd.dev.spring6di.controllers;
 import mohaafaridd.dev.spring6di.services.GreetingServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class ConstructorInjectedControllerTest {
+    @Autowired
     ConstructorInjectedController constructorInjectedController;
-
-    @BeforeEach
-    void setUp() {
-        constructorInjectedController = new ConstructorInjectedController(new GreetingServiceImpl());
-    }
 
     @Test
     void sayHello() {
